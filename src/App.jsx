15ccdb6ca09d8f207cf4c1554a9caec576298f6d
seungjjun <il1uso@naver.com>
2022-09-26 16:51:06 +0900

    styled component 세팅 및 reset css 적용 - 전체에 border-box 스타일 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 
+import { Reset } from 'styled-reset';
+
+import styled from 'styled-components';
+
 import Header from './components/Header';
 
 import AccountPage from './pages/AccountPage';
@@ -7,16 +11,26 @@ import HomePage from './pages/HomePage';
 import TransferPage from './pages/TransferPage';
 import TransactionsPage from './pages/TransactionsPage';
 
+import GlobalStyle from './styles/GlobalStyle';
+
+const Main = styled.main`
+  padding: 1em;
+`;
+
 export default function App() {
   return ((
     <div>
+      <Reset />
+      <GlobalStyle />
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/account" element={<AccountPage />} />
-        <Route path="/transfer" element={<TransferPage />} />
-        <Route path="/transactions" element={<TransactionsPage />} />
-      </Routes>
+      <Main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="/transfer" element={<TransferPage />} />
+          <Route path="/transactions" element={<TransactionsPage />} />
+        </Routes>
+      </Main>
     </div>
   ));
 }
