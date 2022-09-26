90954739ad5c61c025cd94c924251d725dce9561
seungjjun <il1uso@naver.com>
2022-09-26 16:25:34 +0900

    송금과 거래내역의 기본적인 인수 테스트 통과

--- src/pages/TransactionsPage.jsx
+++ src/pages/TransactionsPage.jsx
@@ -1,5 +1,20 @@
 export default function TransactionsPage() {
   return (
-    <p>Transactions!</p>
+    <table>
+      <thead>
+        <tr>
+          <th>종류</th>
+          <th>계좌번호</th>
+          <th>금액</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>송금</td>
+          <td>56789012</td>
+          <td>3,000원</td>
+        </tr>
+      </tbody>
+    </table>
   );
 }
