ff2bec41344284f5d06334ffbbd32409bdbb2e10
seungjjun <il1uso@naver.com>
2022-09-28 22:13:21 +0900

    로그인 페이지 css 적용

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -9,7 +9,53 @@ import styled from 'styled-components';
 
 import useBankStore from '../hooks/useBankStore';
 
-import PrimaryButton from './ui/PrimaryButton';
+const Container = styled.div`
+  margin: auto;
+  margin-top: 4em;
+  height: 100%;
+  width: 50%;  
+`;
+
+const Title = styled.h2`
+  display: flex;
+  justify-content: center;
+  margin-bottom: .15em;
+  font-size: 3em;
+  font-weight: bold;
+`;
+
+const Form = styled.form`
+  padding-top: 2.2em;
+  border-top: 2px solid #ABD9FF;
+`;
+
+const Input = styled.input`
+  margin: .6em 0;
+  padding: 1.2em .6em;
+  width: 100%;
+  border: 1px solid #A0A0A0;
+  color: #A0A0A0;
+  &:focus {
+    border: 1px solid #ABD9FF;
+  }
+`;
+
+const LoginButton = styled.button`
+  margin-top: 3em;
+  padding: 1.8em 1em;
+  width: 100%;
+  border: 1px solid #ABD9FF;
+  background-color: #ABD9FF;
+  color: #FFF;
+`;
+
+const Registration = styled.button`
+  display: flex;
+  margin: auto;
+  margin-top: 3em;
+  border: none;
+  color: #363636;
+`;
 
 const Error = styled.div`
   color: #f23434d3;
@@ -34,40 +80,52 @@ export default function LoginForm() {
     }
   };
 
+  const handleClick = () => {
+    navagate('/register');
+  };
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <h2>로그인</h2>
-      <div>
-        <label htmlFor="input-account-number">
-          계좌 번호
-        </label>
-        <input
-          id="input-account-number"
-          {...register('accountNumber', { required: true })}
-        />
-        {errors.accountNumber ? (
-          <Error>아이디를 입력해주세요</Error>
+    <Container>
+      <Title>USER LOGIN</Title>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          {/* <label htmlFor="input-account-number" /> */}
+          <Input
+            id="input-account-number"
+            placeholder="아이디(계좌번호)"
+            {...register('accountNumber', { required: true })}
+          />
+          {errors.accountNumber ? (
+            <Error>아이디를 입력해주세요</Error>
+          ) : null}
+        </div>
+        <div>
+          {/* <label htmlFor="input-password">
+            패스워드
+          </label> */}
+          <Input
+            id="input-password"
+            placeholder="비밀번호"
+            type="password"
+            {...register('password', { required: true })}
+          />
+          {errors.password ? (
+            <Error>비밀번호를 입력해주세요</Error>
+          ) : null}
+        </div>
+        {bankStore.isLoginFail ? (
+          <Error>{bankStore.errorMessage}</Error>
         ) : null}
-      </div>
-      <div>
-        <label htmlFor="input-password">
-          패스워드
-        </label>
-        <input
-          id="input-password"
-          type="password"
-          {...register('password', { required: true })}
-        />
-        {errors.password ? (
-          <Error>비밀번호를 입력해주세요</Error>
-        ) : null}
-      </div>
-      {bankStore.isLoginFail ? (
-        <Error>{bankStore.errorMessage}</Error>
-      ) : null}
-      <PrimaryButton type="submit" onClick={() => {}}>
-        로그인
-      </PrimaryButton>
-    </form>
+        <LoginButton type="submit">
+          로그인하기
+        </LoginButton>
+        <Registration
+          type="button"
+          onClick={handleClick}
+        >
+          회원가입
+        </Registration>
+      </Form>
+    </Container>
   );
 }
