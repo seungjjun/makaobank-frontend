721c55d6dadde09d0d3e13d67915f46aae1afca0
seungjjun <il1uso@naver.com>
2022-09-28 23:20:19 +0900

    거래내역 페이지 css 적용

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,39 +1,112 @@
+import styled from 'styled-components';
 import useBankStore from '../hooks/useBankStore';
+
 import numberFormat from '../utils/numberFormat';
+
+const Container = styled.div`
+  margin: auto;
+  margin-top: 4em;
+  height: 100%;
+  width: 50%;  
+`;
+
+const Title = styled.h2`
+  display: flex;
+  justify-content: center;
+  margin-bottom: .15em;
+  font-size: 3em;
+  font-weight: bold;
+`;
+
+const Hr = styled.div`
+  margin: auto;
+  padding-top: 4em;
+  width: 60%;
+  border-top: 2px solid #ABD9FF;
+`;
+
+const Tabel = styled.table`
+  width: 100%;
+`;
+
+const Tr = styled.tr`
+  background-color: #ABD9FF;
+  color: #FFF;
+`;
+
+const Th = styled.th`
+  padding: 1em 2em;
+  border-left: 1px solid #D8D8D8;
+  border-right: 1px solid #D8D8D8;
+`;
+
+const P = styled.p`
+  display: flex;
+  justify-content: center;
+  padding: 2em;
+  color: #A0A0A0;
+`;
+
+const List = styled.tr`
+  color: #A0A0A0;
+  border-bottom: 1px solid #A0A0A0;
+`;
+
+const Activity = styled.th`
+  padding-right: 1.7em;
+  border-bottom: 1px solid #D8D8D8;
+  border-right: 1px solid #D8D8D8;
+`;
+const Name = styled.th`
+  padding: 1em 3em;
+  border-bottom: 1px solid #D8D8D8;
+  border-right: 1px solid #D8D8D8;
+`;
+
+const Amount = styled.th`
+  border-bottom: 1px solid #D8D8D8;
+`;
 
 export default function Transactions() {
   const bankStore = useBankStore();
 
   const { transactions } = bankStore;
 
-  if (!transactions.length) {
-    return (
-      <p>
-        거래 내역이 없습니다
-      </p>
-    );
-  }
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>종류</th>
-          <th>계좌번호</th>
-          <th>금액</th>
-        </tr>
-      </thead>
-      <tbody>
-        {transactions.map((transaction) => (
-          <tr key={transaction.id}>
-            <td>{transaction.activity}</td>
-            <td>{transaction.name}</td>
-            <td>
-              {numberFormat(transaction.amount)}
-              원
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <Container>
+      <Title>거래내역</Title>
+      <Hr />
+      <Tabel>
+        <thead>
+          <Tr>
+            <th>종류</th>
+            <Th>계좌번호</Th>
+            <th>금액</th>
+          </Tr>
+        </thead>
+        <tbody>
+          {transactions.length === 0 ? (
+            <List>
+              <th />
+              <P>
+                거래 내역이 없습니다
+              </P>
+            </List>
+          ) : (
+            transactions.map((transaction) => (
+              <List key={transaction.id}>
+                <Activity>{transaction.activity}</Activity>
+                <Name>{transaction.name}</Name>
+                <Amount>
+                  {numberFormat(transaction.amount)}
+                  원
+                </Amount>
+              </List>
+            ))
+          )}
+        </tbody>
+      </Tabel>
+    </Container>
+
   );
 }
